2a1a4a8f81aae867226ffb7d2a40db90597c592f
Linwei Zhang <lewiszlw520@gmail.com>
2021-10-19 20:52:20 +0800

    Study how to read configuration from spring cloud config server

--- hello-world-java-projects/spring-cloud-demo/consumer-service-demo/src/main/java/com/github/lewiszlw/springclouddemo/consumerservice/controller/ApiController.java
+++ hello-world-java-projects/spring-cloud-demo/consumer-service-demo/src/main/java/com/github/lewiszlw/springclouddemo/consumerservice/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.github.lewiszlw.springclouddemo.consumerservice.controller;
 
 import com.github.lewiszlw.springclouddemo.consumerservice.remote.HelloRemote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,16 @@ public class ApiController {
     @Autowired
     private HelloRemote helloRemote;
 
+    @Value("${consumer.hello}")
+    private String consumerHelloConfig;
+
     @RequestMapping("/test-call")
     public String testCall() {
         return helloRemote.hello("lewis");
+    }
+
+    @RequestMapping("/test-config")
+    public String testConfig() {
+        return consumerHelloConfig;
     }
 }
