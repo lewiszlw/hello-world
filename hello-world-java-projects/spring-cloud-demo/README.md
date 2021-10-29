472bc22640219c12205ef62903f33a33a37aa871
Linwei Zhang <lewiszlw520@gmail.com>
2021-10-29 18:55:18 +0800

    Study actuator refresh and configure rabbitmq

--- hello-world-java-projects/spring-cloud-demo/README.md
+++ hello-world-java-projects/spring-cloud-demo/README.md
@@ -1,4 +1,11 @@
 # Spring Cloud Demo
 
-## Spring Cloud Config
+## config-server-demo
 本地启动服务后，可访问 http://localhost:8001/consumer-service-demo/prod 来查看配置。
+
+启动 rabbitmq docker 容器
+`docker run -d --hostname rabbit-host1 --name rabbitmq -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=123456 -p 15672:15672 -p 5672:5672 -p 25672:25672 -p 61613:61613 -p 1883:1883 rabbitmq:management`
+可通过 http://localhost:15672/ 访问 rabbitmq 控制台
+
+## consumer-service-demo
+本地启动服务后，可执行 `curl -X POST http://localhost:9001/actuator/refresh` 来刷新配置。
--- hello-world-java-projects/spring-cloud-demo/consumer-service-demo/src/main/java/com/github/lewiszlw/springclouddemo/consumerservice/controller/ApiController.java
+++ hello-world-java-projects/spring-cloud-demo/consumer-service-demo/src/main/java/com/github/lewiszlw/springclouddemo/consumerservice/controller/ApiController.java
@@ -3,10 +3,12 @@ package com.github.lewiszlw.springclouddemo.consumerservice.controller;
 import com.github.lewiszlw.springclouddemo.consumerservice.remote.HelloRemote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ApiController {
 
     @Autowired
