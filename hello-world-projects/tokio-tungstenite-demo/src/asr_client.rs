8d8322efa1c479188a0f164c453bfc77080e303a
Linwei Zhang <lewiszlw520@gmail.com>
2022-11-11 11:07:10 +0800

    Add asr client server

---
+++ hello-world-projects/tokio-tungstenite-demo/src/asr_client.rs
@@ -0,0 +1,56 @@
+//! A simple example of hooking up stdin/stdout to a WebSocket stream.
+//!
+//! This example will connect to a server specified in the argument list and
+//! then forward all data read on stdin to the server, printing out all data
+//! received on stdout.
+//!
+//! Note that this is not currently optimized for performance, especially around
+//! buffer management. Rather it's intended to show an example of working with a
+//! client.
+//!
+//! You can use this example together with the `server` example.
+
+use std::env;
+
+use futures_util::{future, pin_mut, StreamExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+
+#[tokio::main]
+async fn main() {
+    let connect_addr =
+        env::args().nth(1).unwrap_or_else(|| panic!("this program requires at least one argument"));
+
+    let url = url::Url::parse(&connect_addr).unwrap();
+
+    let (sender, receiver) = futures_channel::mpsc::unbounded();
+    tokio::spawn(read_audio(sender));
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
+
+    let (ws_write, ws_read) = ws_stream.split();
+
+    // mpsc receiver 重定向到 ws_write
+    let receiver_to_ws = receiver.map(Ok).forward(ws_write);
+    
+    let ws_to_stdout = {
+        ws_read.for_each(|message| async {
+            let data = message.unwrap().into_data();
+            tokio::io::stdout().write_all(&data).await.unwrap();
+        })
+    };
+
+    pin_mut!(receiver_to_ws, ws_to_stdout);
+    future::select(receiver_to_ws, ws_to_stdout).await;
+}
+
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+async fn read_audio(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    loop {
+        // 读取asr音频数据
+        let mut buf = vec![1; 1024];
+        tx.unbounded_send(Message::binary(buf)).unwrap();
+    }
+}
---
+++ hello-world-projects/tokio-tungstenite-demo/src/asr_server.rs
@@ -0,0 +1,71 @@
+use futures_util::{SinkExt, StreamExt};
+use log::*;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{accept_async, tungstenite::Error};
+use tokio_tungstenite::tungstenite::Message;
+
+/// 返回asr识别结果
+
+#[tokio::main]
+async fn main() {
+    // 配置日志实现
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    // 监听端口
+    let addr = "127.0.0.1:9003";
+    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+    info!("Listening on: {}", addr);
+
+    while let Ok((stream, client_addr)) = listener.accept().await {
+        info!("Client address: {}", client_addr);
+
+        // spawn 创建一个 Tokio 任务（一个异步的绿色线程），spawn 函数的参数是一个 async 语句块
+        tokio::spawn(accept_connection(client_addr, stream));
+    }
+}
+
+async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
+    // websocket握手
+    let mut ws_stream = accept_async(stream).await.expect("Failed to accept ws");
+    info!("New WebSocket connection: {}", peer);
+
+    while let Some(item) = ws_stream.next().await {
+        match item {
+            Ok(msg) => {
+                match msg {
+                    Message::Text(text) => {
+                        info!("Received text message: {}", text);
+                        if text.contains("RecognitionStart") {
+                            info!("Received RecognitionStart message");
+                        }
+                    },
+                    Message::Binary(data) => {
+                        info!("Received binary message, size {}", data.len());
+                        // 发送asr识别结果
+                        ws_stream.send(Message::Text("asr识别结果".to_string())).await.unwrap();
+                    }
+                    Message::Close(_) => {
+                        info!("Received close message.");
+                        if let Err(e) = ws_stream.close(None).await {
+                            match e {
+                                Error::ConnectionClosed => (),
+                                _ => {
+                                    info!("Error while closing: {:?}", e);
+                                    break;
+                                },
+                            }
+                        }
+                    }
+                    _ => (),
+                }
+            }
+            Err(e) => {
+                error!("Error receiving message: {:?}", e);
+            }
+        }
+    }
+}
