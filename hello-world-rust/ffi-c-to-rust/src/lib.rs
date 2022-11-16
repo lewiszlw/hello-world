d7da7cb5a5f02f68a8acf035aed8ae5e9336fbb2
Linwei Zhang <lewiszlw520@gmail.com>
2022-11-16 16:21:56 +0800

    Pass Box and Rc between C and Rust

--- hello-world-rust/ffi-c-to-rust/src/lib.rs
+++ hello-world-rust/ffi-c-to-rust/src/lib.rs
@@ -1,6 +1,80 @@
 #![crate_type = "staticlib"]
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
 #[no_mangle]
 pub extern "C" fn double_input(input: i32) -> i32 {
     input * 2
+}
+
+#[repr(C)]
+pub struct Channel {
+    pub id: *const libc::c_char,
+    pub obj: *mut libc::c_void,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct ChannelObj {
+    pub name: String,
+    pub active: bool,
+    pub data: Vec<u8>,
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn create_channel_obj(mut channel: *mut Channel) {
+    let channel_obj = ChannelObj {
+        name: "ChannelObj".to_string(),
+        active: true,
+        data: vec![1 as u8; 5],
+    };
+    let mut channel_obj = Box::new(channel_obj);
+    (*channel).obj = Box::into_raw(channel_obj) as *mut libc::c_void;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn print_channel_obj(mut channel: *mut Channel) {
+    let channel_obj = Box::from_raw((*channel).obj as *mut ChannelObj);
+    println!("channel_obj: {:?}", channel_obj);
+    // 防止Box被drop掉
+    std::mem::forget(channel_obj);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn create_channel_obj_rc(mut channel: *mut Channel) {
+    let channel_obj = ChannelObj {
+        name: "ChannelObj-Rc".to_string(),
+        active: true,
+        data: vec![2 as u8; 5],
+    };
+    let mut channel_obj = Rc::new(channel_obj);
+    (*channel).obj = Rc::into_raw(channel_obj) as *mut libc::c_void;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn print_channel_obj_rc(mut channel: *mut Channel) {
+    let channel_obj = Rc::from_raw((*channel).obj as *mut ChannelObj);
+    println!("channel_obj_rc: {:?}", channel_obj);
+    // 防止Rc被drop掉
+    std::mem::forget(channel_obj);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn create_channel_obj_box_rc(mut channel: *mut Channel) {
+    let channel_obj = ChannelObj {
+        name: "ChannelObj-Box-Rc".to_string(),
+        active: true,
+        data: vec![2 as u8; 5],
+    };
+    let mut channel_obj = Rc::new(RefCell::new(channel_obj));
+    (*channel).obj = Box::into_raw(Box::new(channel_obj)) as *mut libc::c_void;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn print_channel_obj_box_rc(mut channel: *mut Channel) {
+    let mut channel_obj = &mut *((*channel).obj as *mut Rc<RefCell<ChannelObj>>);
+    println!("channel_obj_box_rc: {:?}", channel_obj);
+    // 不会被drop掉
+    // 但需要手动将Rc引用计数置0来进行drop
 }
--- hello-world-rust/ffi-c-to-rust/src/main.c
+++ hello-world-rust/ffi-c-to-rust/src/main.c
@@ -1,11 +1,59 @@
 #include <stdint.h>
 #include <stdio.h>
 
+struct Channel
+{
+    char *id;
+    void *obj;
+};
+
 extern int32_t double_input(int32_t input);
+
+extern create_channel_obj(struct Channel*);
+extern print_channel_obj(struct Channel*);
+
+extern create_channel_obj_rc(struct Channel*);
+extern print_channel_obj_rc(struct Channel*);
+
+extern create_channel_obj_box_rc(struct Channel*);
+extern print_channel_obj_box_rc(struct Channel*);
 
 int main() {
     int input = 4;
     int output = double_input(input);
     printf("%d * 2 = %d\n", input, output);
+
+    // create channel
+    struct Channel channel;
+    channel.id = "channel_id";
+    struct Channel *channel_ptr = &channel;
+    printf("channel id: %s\n", channel_ptr->id);
+
+    create_channel_obj(channel_ptr);
+
+    print_channel_obj(channel_ptr);
+    print_channel_obj(channel_ptr);
+
+    // create channel2
+    struct Channel channel2;
+    channel2.id = "channel_id2";
+    struct Channel *channel_ptr2 = &channel2;
+    printf("channel id: %s\n", channel_ptr2->id);
+
+    create_channel_obj_rc(channel_ptr2);
+
+    print_channel_obj_rc(channel_ptr2);
+    print_channel_obj_rc(channel_ptr2);
+
+    // create channel3
+    struct Channel channel3;
+    channel3.id = "channel_id3";
+    struct Channel *channel_ptr3 = &channel3;
+    printf("channel id: %s\n", channel_ptr3->id);
+
+    create_channel_obj_box_rc(channel_ptr3);
+
+    print_channel_obj_box_rc(channel_ptr3);
+    print_channel_obj_box_rc(channel_ptr3);
     return 0;
 }
